4eeb0a01458e1eb477e2b22c6a4bf40931e59836
Lewis <ALewis2@bcbsm.com>
2018-10-31 20:03:27 -0400

    JS DONE!

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 let eventType = prompt("What are you doing tonight?");
 let tempFahr = prompt("What's the Temperature");
-
+let resultEvent =""
+let resultTemperature=""
 
 if(eventType==="formal") {
-    console.log("You should wear a suit")
+    resultEvent=("since it is ") + tempFahr + " degrees, you should wear a suit "
 }
 
 else if(eventType==="semiformal") {
-    console.log("You should wear a Polo")
+    resultEvent=("since it is ") + tempFahr + " degrees, you should wear a polo "
 }
 
 else if(eventType==="casual") {
-    console.log("you should wear something comfy")
+    resultEvent=("since it is ") + tempFahr + " degrees, you should wear something comfy "
 }
 
 if(tempFahr > 70) {
-    console.log("you don't need a jacket")
+    tempFahr=("and no jacket")
 }
 
 else if (tempFahr <= 70 && tempFahr >= 54) {
-    console.log("you should wear a jacket")
+    tempFahr=("and wear a jacket")
 }
 
 else if(tempFahr < 54) {
-    console.log("you should wear a coat");
+    tempFahr=("and wear a coat");
 }
+
+
+let result = resultEvent += tempFahr; 
+    console.log(result)
